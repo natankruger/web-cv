64ad40cc71732dbca2a1e00a4cfd48c7eb37a101
Natan Kruger <natan.kruger@zygotecnologia.com>
2020-05-01 09:48:57 -0300

    Styles small configuration, adding sass and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import './App.css';
+
+import 'bootstrap';
+import './styles/application.scss';
 
 function App() {
   return (
-    <div className="App">
+    <section className="App">
       <header className="App-header">
+      </header>
+      <section className="app-body">
         <h1>
           Em construção.
         </h1>
         <h2>
           Under construction.
         </h2>
-      </header>
-    </div>
+      </section>
+    </section>
   );
 }
 
