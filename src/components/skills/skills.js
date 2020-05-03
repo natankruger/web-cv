636b68f68329567e602532d62662728c82e7e4dc
Natan Kruger <natan.kruger@zygotecnologia.com>
2020-05-03 11:51:07 -0300

    Adding a skills component

---
+++ src/components/skills/skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Skills extends React.Component {
+
+  render() {
+
+    return <section className="skills">
+      <ul className="skills-list">
+        <li></li>
+      </ul>
+    </section>
+  }
+}
+
+export default Skills;
