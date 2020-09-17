48703cb3c30f9f1989685dfe9cba2d21e5b01b10
Nata Krüger <natan.kruger@gmail.com>
2020-09-16 21:35:48 -0300

    Adding fake data library

--- src/pages/responsiveTable/responsiveTable.js
+++ src/pages/responsiveTable/responsiveTable.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import faker from 'faker';
 
 class ResponsiveTable extends React.Component {
 
   render() {
 
-    return <div>
-      Hello world
-    </div>
+    return <section className="mx-auto">
+    <table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Sobrenome</th>
+          <th>Aniversário</th>
+          <th>email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{ faker.name.firstName() }</td>
+          <td>{ faker.name.lastName() }</td>
+          <td>{ faker.date.past().toDateString() }</td>
+          <td>{ faker.internet.email() }</td>
+        </tr>
+      </tbody>
+    </table>
+    </section>
   }
 }
 
